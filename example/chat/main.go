cd3b19b49a44d10a97c25c7fa5ec07496ed51ebc
zhoujun <dev.zhoujun@gmail.com>
2016-12-02 01:38:52 +0800

    run chat server

--- example/chat/main.go
+++ example/chat/main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+	endpoints := []string{
+		"127.0.0.1:2379",
+	}
+
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	name := hex.EncodeToString(b)
+	fmt.Println(name)
+
+	log.Fatal(RunServer(endpoints, name, callback))
+}
+
+func callback(name string, send net.Conn, receive net.Conn) error {
+	if send != nil {
+		fmt.Println(name, send.RemoteAddr().String())
+	}
+	if receive != nil {
+		fmt.Println(name, receive.RemoteAddr().String())
+	}
+	return nil
 }
--- example/chat/server.go
+++ example/chat/server.go
@@ -2,37 +2,104 @@ package main
 
 import (
 	"net"
+
+	"time"
+
+	"github.com/4396/etcdd"
 )
 
-type server struct {
-	name     string
-	addr     string
-	listener net.Listener
-}
+const namespace = "/services/chatroom/"
 
-func newServer(name, addr string) (s *server) {
-	s = &server{
-		name: name,
-		addr: addr,
-	}
-	return s
-}
+type Callback func(name string, send net.Conn, receive net.Conn) error
 
-func (s *server) Run(handle func(conn net.Conn) error) (err error) {
-	l, err := net.Listen("tcp", s.addr)
+// RunServer listen tcp port and register service
+func RunServer(endpoints []string, name string, callback Callback) (err error) {
+	// listen tcp port
+	l, err := net.Listen("tcp", "127.0.0.1:")
 	if err != nil {
 		return
 	}
-	for {
-		var conn net.Conn
-		conn, err = l.Accept()
-		if err != nil {
-			break
+	defer l.Close()
+
+	receive := make(chan net.Conn, 16)
+	cherr := make(chan error, 1)
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				cherr <- err
+				break
+			}
+			receive <- conn
 		}
-		err = handle(conn)
+	}()
+
+	// register, keepalive and watch service
+	d, err := etcdd.NewV3(endpoints)
+	if err != nil {
+		return
+	}
+
+	svrs, err := d.Services(namespace)
+	if err != nil {
+		return
+	}
+	for name, addr := range svrs {
+		var conn net.Conn
+		conn, err = net.Dial("tcp", addr)
 		if err != nil {
-			break
+			return
+		}
+		err = callback(name, conn, nil)
+		if err != nil {
+			return
 		}
 	}
-	return
+
+	timeout := time.Second * 5
+	keepalive, err := d.Register(namespace, name, l.Addr().String(), timeout*2)
+	if err != nil {
+		return
+	}
+	defer d.Unregister(namespace, name)
+
+	event, cancel, err := d.Watch(namespace)
+	if err != nil {
+		return
+	}
+	defer cancel()
+
+	ticker := time.NewTicker(timeout)
+	defer ticker.Stop()
+	for {
+		select {
+		case err = <-cherr:
+			return
+		case <-ticker.C:
+			err = keepalive()
+			if err != nil {
+				return
+			}
+		case ev := <-event:
+			if ev.Action == "put" {
+				var conn net.Conn
+				conn, err = net.Dial("tcp", ev.Addr)
+				if err != nil {
+					return
+				}
+				err = callback(ev.Name, conn, nil)
+				if err != nil {
+					return
+				}
+			} else if ev.Action == "delete" {
+
+			}
+		case conn := <-receive:
+			err = callback("", nil, conn)
+			if err != nil {
+				return
+			}
+		}
+	}
 }
